27ac889f4d28650e92977365a6f6b1948c2d41b0
veggiss <veg_ulf@hotmail.com>
2018-05-25 20:38:38 +0200

    Damn H15 idle

--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
 const port = process.env.PORT || 3000;
 const path = require('path');
 const express = require('express');
-const timeout = require('connect-timeout');
 const http = require('http');
 const colyseus = require("colyseus");
 const game = require("./rooms/game");
@@ -9,17 +8,17 @@ const uws = require('uws');
 const app = express();
 
 //app.enable('trust proxy');
-app.set('port', (port));
-app.use(timeout(30000));
+app.set('port', port);
 app.use(express.static('./static'));
 
 const server = http.createServer(app);
-const gameServer = new colyseus.Server({
-	engine: uws.Server,
-	server: server,
-    pingInterval: 15000,
-    pingTimeout: 30000
-});
+const wss = new uws.Server({port: 1337});
+const gameServer = new colyseus.Server();
+
+gameServer.attach({ws: wss});
+gameServer.attach({server: server});
+
+console.log(wss);
 
 gameServer.register("game", game);
 
