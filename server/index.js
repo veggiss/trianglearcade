db8d2366c490295ff5723b1fe27508234e432071
veggiss <veg_ulf@hotmail.com>
2018-05-25 20:58:32 +0200

    Changed server listener

--- server/index.js
+++ server/index.js
@@ -8,20 +8,14 @@ const uws = require('uws');
 const app = express();
 
 //app.enable('trust proxy');
-app.set('port', port);
+app.set('port', (port));
 app.use(express.static('./static'));
 
-const server = http.createServer(app);
-const wss = new uws.Server({port: 1337});
-const gameServer = new colyseus.Server();
-
-gameServer.attach({ws: wss});
-gameServer.attach({server: server});
-
-console.log(wss);
+const gameServer = new colyseus.Server({
+	engine: uws.Server,
+	server: http.createServer(app)
+});
 
 gameServer.register("game", game);
 
-server.listen(port, () => {
-  console.log(`-> Server started on port ${port}<-`);
-});
+gameServer.listen(port);
